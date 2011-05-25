cbda0659dcc447823e830afe8caa3e51b0c1110d
Tomasz Trela <tomas.trela@gmail.com>
2011-05-25 13:11:26 +0200

    Added nice testmethod decorator to enable set of features per test

--- featureflipper/tests.py
+++ featureflipper/tests.py
@@ -3,47 +3,76 @@ from django.http import HttpRequest
 from django.test.client import Client
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Permission
+from django.core.urlresolvers import reverse
 
 from featureflipper.models import Feature
 from featureflipper.middleware import FeaturesMiddleware
 
-class featureflipperTest(TestCase):
+from functools import wraps
+
+def enable_features(features = []):
+    """Make sure all features required for test are enabled on site level."""
+    def outter_wrapper(f):
+        @wraps(f)
+        def wrapper(self, *args, **kwargs):
+            Feature.objects.filter(name__in = features).update(enabled=True)
+            return f(self, *args, **kwargs)
+        return wrapper
+    return outter_wrapper
+
+class FeatureFlipperTest(TestCase):
     """
     Tests for django-feature-flipper
     """
-    def test_something(self):
-        feature = Feature.objects.create(name='fftestfeature')
-        user = User.objects.create_user('fftestuser', '', 'password')
+    def setUp(self):
 
-        c = Client()
-        self.assertTrue(c.login(username='fftestuser', password='password'))
+        self.feature = Feature.objects.create(name='fftestfeature')
+        self.user = User.objects.create_user('fftestuser', '', 'password')
 
-        response = c.get('/')
+        self.client = Client()
+        self.assertTrue(self.client.login(username='fftestuser', password='password'))
+
+    def test_middleware(self):
+        """Test features object is correctly set on request.
+        No features are enabled
+        """
+
+        response = self.client.get('/')
+
         self.assertTrue('features' in response.context)
         self.assertTrue('fftestfeature' in response.context['features'])
         self.assertFalse(response.context['features']['fftestfeature'])
 
-        response = c.get('/?enable_fftestfeature')
+    def test_enable_feature_per_request(self):
+        """Param ?enable_fftestfeature enables particular
+           feature only for the current request"""
+        response = self.client.get('/?enable_fftestfeature')
         self.assertTrue(response.context['features']['fftestfeature'])
 
-        response = c.get('/')
+        response = self.client.get('/')
         self.assertFalse(response.context['features']['fftestfeature'])
 
-        response = c.get('/?session_enable_fftestfeature')
+    def test_enable_feature_per_session(self):
+
+        response = self.client.get('/?session_enable_fftestfeature')
         self.assertFalse(response.context['features']['fftestfeature'])
 
         perm = Permission.objects.get(codename='can_flip_with_url')
-        user.user_permissions.add(perm)
+        self.user.user_permissions.add(perm)
+        self.assertTrue(self.user.has_perm('featureflipper.can_flip_with_url'))
 
-        self.assertTrue(user.has_perm('featureflipper.can_flip_with_url'))
-        response = c.get('/?session_enable_fftestfeature')
+        response = self.client.get('/?session_enable_fftestfeature')
 
         self.assertTrue(response.context['features']['fftestfeature'])
-        response = c.get('/')
+        response = self.client.get('/')
         self.assertTrue(response.context['features']['fftestfeature'])
 
-        response = c.get('/?session_clear_features')
+        response = self.client.get('/?session_clear_features')
         self.assertFalse(response.context['features']['fftestfeature'])
 
-        feature.delete()
-        user.delete()
+
+    def test_no_deco(self):
+        self.assertFalse(Feature.objects.get(name='fftestfeature').enabled)
+    @enable_features(["fftestfeature"])
+    def test_anable_feature_decorator(self):
+        self.assertTrue(Feature.objects.get(name='fftestfeature').enabled)
